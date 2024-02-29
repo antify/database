65eedb70a270a4c2dcde979d4d16c3d0c0ad2361
Johannes Beyer <johannes@blue-panda.de>
2024-02-29 09:07:06 +0100

    chore: add codex workflows

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,31 @@
 module.exports = {
-  'extends': ['@nuxt/eslint-config'],
-  'rules': {
-    'no-extra-semi': 0,
-    'vue/html-self-closing': 0,
-    'vue/singleline-html-element-content-newline': 0
-  }
+	root: true,
+	extends: [
+		'@nuxt/eslint-config',
+		'plugin:storybook/recommended'
+	],
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx', '*.mts', '*.cts', '*.vue'],
+			rules: {
+				quotes: ['warn', 'single', {
+					avoidEscape: true
+				}],
+				'prefer-promise-reject-errors': 'error',
+				'no-extra-semi': 0,
+				'vue/html-self-closing': 0,
+				'vue/singleline-html-element-content-newline': 0,
+				"vue/component-name-in-template-casing": ["error", "PascalCase", {
+					"registeredComponentsOnly": false
+				}],
+				'@typescript-eslint/explicit-function-return-type': 'off',
+				'@typescript-eslint/no-explicit-any': 'warn',
+				'@typescript-eslint/no-misused-promises': 'error',
+				'@typescript-eslint/no-unnecessary-type-assertion': 'error',
+			},
+			parserOptions: {
+				project: './tsconfig.json',
+			},
+		},
+	]
 };
